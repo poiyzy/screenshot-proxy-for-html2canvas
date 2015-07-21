0a682a86fd243a53c5c4302574c0dc1f80d53af6
Roy Young <zhuoyuyang@gmail.com>
2015-07-21 23:43:33 +0800

    Fit for growing.io.

--- __tests__/image_fetcher_test.js
+++ __tests__/image_fetcher_test.js
@@ -5,10 +5,18 @@ jest.dontMock('../lib/image_fetcher.js');
 describe("ImageFetcher#getImage", function() {
   it("calls the request method", function() {
     var ImageFetcher = require('../lib/image_fetcher.js');
+    var request = require('request');
 
     var fetcher = new ImageFetcher('http://test.url');
+     var callback = jest.genMockFunction();
     fetcher.getImage(callback);
 
-    expect(callback).toBeCalledWith(["no error", {statusCode: 200}, "image data"]);
+    expect(request).toBeCalledWith({
+      url: 'http://test.url',
+      method: 'GET',
+      encoding: 'base64',
+      timepit: 60*1000
+    }, callback);
+      // ["no error", {statusCode: 200}, "image data"]);
   });
 });
